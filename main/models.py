47a243502d9f75af32dea11505ba91f049da5f14
Daniel <buzurmankulovdaniela@gmail.com>
2026-09-23 18:53:25 +0600

    +

--- main/models.py
+++ main/models.py
@@ -40,3 +40,10 @@ class Teacher(models.Model):
 
     def __str__(self):
         return self.name
+
+class Studentt(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    email = models.EmailField(unique=True)
+    def __str__(self):
+        return self.name
